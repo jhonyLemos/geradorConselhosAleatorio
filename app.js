ea44812a0532c8949506eec4905819209178cc3c
Jhony Lemos <jhone.analistadesistemas@gmail.com>
2022-07-23 23:11:13 -0300

    Gerador de conselhos aleatorio

---
+++ app.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector(".container .btn .btn_acao");
+let title = document.querySelector(".container .id-advice .title-adv");
+const text = document.querySelector(".container .content .advice-text");
+
+btn.addEventListener("click", function () {
+  axios
+    .get("https://api.adviceslip.com/advice")
+    .then((response) => {
+      title.innerHTML = "Advice #" + response.data.slip.id;
+      text.innerHTML = "''" + response.data.slip.advice + "''";
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+    .then(() => {
+      console.log("Codigo de Jhony Lemos");
+    });
+});
